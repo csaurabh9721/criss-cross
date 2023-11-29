044a099ad9bd4dfb33913b3f87471b9ecf1851f2
sarvesh <lucksam38@gmail.com>
2023-11-30 00:25:04 +0530

    add logo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Criss Cross',
+      title: 'CrisCrass',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
